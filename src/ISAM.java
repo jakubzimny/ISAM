d366ba999ca85cad4327e22408e1fac05b58129c
jakubzimny <jakubzimny@outlook.com>
2018-12-18 00:53:57 +0100

    inserting, printing, reorganizing done

--- src/ISAM.java
+++ src/ISAM.java
@@ -1,12 +1,14 @@
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.Random;
+
 
 public class ISAM {
 
     private final int BLOCKING_FACTOR = 10;
-    private final int RECORD_SIZE = 60+4+4+1;
+    private final int RECORD_SIZE = 60 + 4 + 4 + 1;
+    private final double ALPHA = 0.6;
+    private final double PRIMARY_TO_OVERFLOW_RATIO = 0.4;
 
     public ArrayList<IndexEntry> getIndex() {
         return index;
@@ -14,179 +16,286 @@ public class ISAM {
 
     private ArrayList<IndexEntry> index;
 
-    private String dataFileName = "data.dat";
-    private String indexFileName = "index.dat";
+    private String dataFile = "data.dat";
+    private String indexFile = "index.dat";
+    private String tempDataFile = "tempData.dat";
 
-    public String getDataFileName() {
-        return dataFileName;
-    }
-
-    public ISAM(){
+     ISAM() {
         index = new ArrayList<>();
-        File tmpFile = new File(indexFileName);
-        if (tmpFile.exists())loadIndex();
+        File tmpFile = new File(indexFile);
+        if (tmpFile.exists()) loadIndex();
     }
 
-    public void generateRandom(int n){
+    public void generateRandom(int n) {
 
     }
 
-    private void loadIndex(){
-        //TODO load index file from disk
+    public void saveIndex() {
+        File f = new File(indexFile);
+        if(index.size()!=0) {
+            try (FileOutputStream fos = new FileOutputStream(f);
+                 DataOutputStream dos = new DataOutputStream(fos)) {
+                for (IndexEntry ie : index) {
+                    dos.writeInt(ie.getKey());
+                    dos.writeInt(ie.getPageNo());
+                }
+            } catch (IOException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
     }
 
-    public long getPagesNumber(){
-        File f = new File(dataFileName);
-        return (f.length()/RECORD_SIZE)/BLOCKING_FACTOR;
+    private void loadIndex() {
+        File f = new File(indexFile);
+        try (FileInputStream fis = new FileInputStream(f);
+             DataInputStream dis = new DataInputStream(fis)) {
+            while (true) {
+                int key = dis.readInt();
+                int pageNo = dis.readInt();
+                index.add(new IndexEntry(key, pageNo));
+            }
+        } catch (EOFException eof) {
+            //EOF
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
     }
 
-    private void assignKey(Record r){
-//        int key=0; //key is sum of all ASCII codes in record - offset
-//        char[] charset = r.getCharset();
-//        for (char c : charset){
-//            key+=c;
-//        }
-//        key -= 32;
-//        r.setKey(key);
+    public int getPagesNumber() {
+        File f = new File(dataFile);
+        return (int)(f.length() / RECORD_SIZE) / BLOCKING_FACTOR;
+    }
+
+    private void assignKey(Record r) {
         r.setKey(new String(r.getCharset()).hashCode());
     }
 
-    public void reorganize(){
-        //TODO reorganize handling
+    public void reorganize() {
+        int pageCounter=1;
+        ArrayList<IndexEntry> newIndex = new ArrayList<>();
+        ArrayList<Record> newPageBuffer = new ArrayList<>();
+        ArrayList<Record> overflowPage = new ArrayList<>();
+        for(int i= 1; i<= index.size(); i++){
+            ArrayList<Record> pageBuffer = getPage(i);
+            if(i==1) newIndex.add(new IndexEntry(pageBuffer.get(0).getKey(),pageCounter));
+            if(newIndex.get(pageCounter-1).getKey() == -1)
+                newIndex.get(pageCounter-1).setKey(pageBuffer.get(0).getKey());
+            int lastOAPageNo = -1;
+            for (Record r : pageBuffer){
+                int offset = r.getOffsetPointer();
+                r.setOffsetPointer(-1);
+                newPageBuffer.add(r);
+                while(offset != -1){
+                    if(newPageBuffer.size() >= (int)Math.ceil(BLOCKING_FACTOR* ALPHA)){
+                        savePage(newPageBuffer, pageCounter, tempDataFile);
+                        pageCounter++;
+                        newIndex.add(new IndexEntry(-1,pageCounter));
+                        newPageBuffer.clear();
+                    }
+                    if(calculatePageNumber(offset) != lastOAPageNo)
+                        overflowPage = getPage(calculatePageNumber(offset));
+                    lastOAPageNo = calculatePageNumber(offset);
+                    Record record = overflowPage.get(calculatePageOffset(offset));
+                    offset = record.getOffsetPointer();
+                    record.setOffsetPointer(-1);
+                    if(newIndex.get(pageCounter-1).getKey() == -1)
+                        newIndex.get(pageCounter-1).setKey(record.getKey());
+                    newPageBuffer.add(record);
+                }
+                if(newPageBuffer.size() >= (int)Math.ceil(BLOCKING_FACTOR* ALPHA)){
+                    savePage(newPageBuffer, pageCounter, tempDataFile);
+                    pageCounter++;
+                    newIndex.add(new IndexEntry(-1,pageCounter));
+                    newPageBuffer.clear();
+                }
+            }
+        }
+        if(newPageBuffer.size()!=0)
+            savePage(newPageBuffer, pageCounter, tempDataFile);
+        if (newIndex.get(newIndex.size()-1).getKey() == -1)
+            newIndex.remove(newIndex.size()-1);
+        index = newIndex;
+        File fData = new File(dataFile);
+        File fIndex = new File(indexFile);
+        File newData = new File(tempDataFile);
+        fData.delete();
+        fIndex.delete();
+        saveIndex();
+        newData.renameTo(new File(dataFile));
+        for (int i = 1; i <= (int)Math.ceil(index.size()*PRIMARY_TO_OVERFLOW_RATIO); i++){
+            savePage(new ArrayList<>(), index.size()+i, dataFile);
+        }
     }
 
-    private int saveToOverflow(Record r){ // save to overflow and return offset pointer
-        int pageNo= -1, ret = -1;
+    private int saveToOverflow(Record r) { // save to overflow and return offset pointer
+        int pageNo = -1, ret = -1;
         ArrayList<Record> page = new ArrayList<>();
-        for(int i =index.size()+1; i<=getPagesNumber(); i++) {
-           page = getPage(i);
+        for (int i = index.size() + 1; i <= getPagesNumber(); i++) {
+            page = getPage(i);
             if (page.size() < BLOCKING_FACTOR) {
                 pageNo = i;
                 break;
             }
         }
-        if(pageNo!=-1) {
+        if (pageNo != -1) {
             page.add(r);
-            ret = page.size()-1 + pageNo*RECORD_SIZE;
-            savePage(page, pageNo);
-        }
-        else System.err.println("Cannot save page to Overflow Area - there is no space left.");
+            ret = (page.size() - 1) * RECORD_SIZE + pageNo * RECORD_SIZE * BLOCKING_FACTOR;
+            savePage(page, pageNo, dataFile);
+        } else System.err.println("Cannot save page to overflow area - there is no space left.");
         return ret;
     }
 
-    public void insert(Record r){
+    public void insert(Record r) {
         //TODO if overflow => reorganize
         assignKey(r);
         ArrayList<Record> pageBuffer = new ArrayList<>();
-        if(index.size()==0){  //index empty
+        if (index.size() == 0) {  //index empty
             pageBuffer.add(r);
             index.add(new IndexEntry(r.getKey(), 1));
-            savePage(pageBuffer,1);
-            savePage(new ArrayList<>(), 2); //1 overflow page at start
+            savePage(pageBuffer, 1, dataFile);
+            savePage(new ArrayList<>(), 2, dataFile); //1 overflow page at start
             return;
         }
-
         int pageNo = indexLookup(r.getKey());
         pageBuffer = getPage(pageNo);
-        if(pageNo == 1){
-            //TODO new smallest record handling
-        }
-        if(pageBuffer.size() >= BLOCKING_FACTOR){ //page full so write in overflow area
-            if(pageBuffer.get(pageBuffer.size()-1).getOffsetPointer() != -1) { //12 -> 13 case
-                r.setOffsetPointer(pageBuffer.get(pageBuffer.size()-1).getOffsetPointer()); //TODO refactoring (DRY violation)
+        for(Record rec: pageBuffer){ // Test if record is unique
+            if(rec.getKey() == r.getKey()){
+                System.err.println("This record already exists.");
+                return;
             }
+        }
+        if (pageNo == 1 && r.getKey() < pageBuffer.get(0).getKey()) { //new smallest record
+            Record temp = pageBuffer.get(0);
+            pageBuffer.set(0, r);
+            int offset = saveToOverflow(temp);
+            pageBuffer.get(0).setOffsetPointer(offset);
+            savePage(pageBuffer, pageNo, dataFile);
+            index.get(0).setKey(r.getKey());
+            return;
+        }
+        if (pageBuffer.size() >= BLOCKING_FACTOR) { //page full so write in overflow area
             int offsetPointer = saveToOverflow(r);
-            pageBuffer.get(pageBuffer.size()-1).setOffsetPointer(offsetPointer);
-            savePage(pageBuffer,pageNo);
-
+            updateOffset(pageBuffer, r, offsetPointer, pageBuffer.size(), pageNo);
         }
-        else{ // empty space in page
-            int placement=pageBuffer.size();
-            for(int i=0; i<pageBuffer.size(); i++ ){
-                if(r.getKey() < pageBuffer.get(i).getKey()){
-                 placement = i;
-                 break;
+        else { // empty space in page
+            int placement = pageBuffer.size();
+            for (int i = 0; i < pageBuffer.size(); i++) {
+                if (r.getKey() < pageBuffer.get(i).getKey()) {
+                    placement = i;
+                    break;
                 }
             }
-            if(placement == pageBuffer.size()){ // if can be placed at the end then insert in primary area
+            if (placement == pageBuffer.size()) { // if can be placed at the end then insert in primary area
                 pageBuffer.add(r);
-                savePage(pageBuffer, pageNo);
-            }
-            else{ // else overflow area
-                if(pageBuffer.get(pageBuffer.size()-1).getOffsetPointer() != -1) { //12 -> 13 case
-                    r.setOffsetPointer(pageBuffer.get(pageBuffer.size()-1).getOffsetPointer()); //TODO refactoring (DRY violation)
-                }
+                savePage(pageBuffer, pageNo, dataFile);
+            } else { // else overflow area
                 int offsetPointer = saveToOverflow(r);
-                pageBuffer.get(placement-1).setOffsetPointer(offsetPointer);
-                savePage(pageBuffer,pageNo);
+                updateOffset(pageBuffer, r, offsetPointer, placement, pageNo);
             }
         }
     }
 
-    private void savePage(ArrayList<Record> page, int pageNo){
-        File f = new File(dataFileName);
+    private void updateOffset(ArrayList<Record> pageBuffer, Record r, int offsetPointer, int placement, int pageNo) {
+        int oldOffset = pageBuffer.get(placement - 1).getOffsetPointer();
+        if (oldOffset != -1) {
+            ArrayList<Record> temp = getPage(calculatePageNumber(oldOffset));
+            int lastPageNo = calculatePageNumber(oldOffset);
+            boolean done = false;
+            Record overflowRecord = temp.get(calculatePageOffset(oldOffset));
+            while (overflowRecord.getKey() < r.getKey()) {
+                if (overflowRecord.getOffsetPointer() == -1) {
+                    overflowRecord.setOffsetPointer(offsetPointer);
+                    temp.set(calculatePageOffset(oldOffset), overflowRecord);
+                    savePage(temp, calculatePageNumber(oldOffset), dataFile);
+                    done = true;
+                    break;
+                }
+                oldOffset = overflowRecord.getOffsetPointer();
+                if (calculatePageNumber(oldOffset) != lastPageNo) {
+                    temp = getPage(calculatePageNumber(oldOffset));
+                    lastPageNo = calculatePageNumber(oldOffset);
+                }
+                overflowRecord = temp.get(calculatePageOffset(oldOffset));
+            }
+            if (!done) {
+                pageBuffer.get(placement - 1).setOffsetPointer(offsetPointer);
+                r.setOffsetPointer(oldOffset);
+                ArrayList<Record> temp2 = getPage(calculatePageNumber(offsetPointer));
+                temp2.set(calculatePageOffset(offsetPointer), r);
+                savePage(temp2, calculatePageNumber(offsetPointer), dataFile);
+            }
+        } else pageBuffer.get(placement - 1).setOffsetPointer(offsetPointer);
+        savePage(pageBuffer, pageNo, dataFile);
+    }
+
+    private int calculatePageNumber(int offsetPointer) {
+        return offsetPointer / (BLOCKING_FACTOR * RECORD_SIZE);
+    }
+
+    private int calculatePageOffset(int offsetPointer) {
+        return (offsetPointer % (BLOCKING_FACTOR * RECORD_SIZE)) / RECORD_SIZE;
+    }
+
+    private void savePage(ArrayList<Record> page, int pageNo, String fileName) {
+        File f = new File(fileName);
         try {
             f.createNewFile();
             RandomAccessFile raf = new RandomAccessFile(f, "rw");
-            raf.seek((pageNo-1) * BLOCKING_FACTOR * RECORD_SIZE );
-            ByteBuffer b = ByteBuffer.allocate(RECORD_SIZE*BLOCKING_FACTOR);
-            for(Record r: page){
+            raf.seek((pageNo - 1) * BLOCKING_FACTOR * RECORD_SIZE);
+            ByteBuffer b = ByteBuffer.allocate(RECORD_SIZE * BLOCKING_FACTOR);
+            for (Record r : page) {
                 b.putInt(r.getKey());
-                for(char c: r.getCharset())b.putChar(c);
-                for(int i=0; i < 30 - r.getCharset().length; i++){
-                    b.put((byte)0x00);
-                    b.put((byte)0x00);
+                for (char c : r.getCharset()) b.putChar(c);
+                for (int i = 0; i < 30 - r.getCharset().length; i++) {
+                    b.put((byte) 0x00);
+                    b.put((byte) 0x00);
                 }
-                b.put((byte)(r.isDeleted()? 0x01 : 0x00));
+                b.put((byte) (r.isDeleted() ? 0x01 : 0x00));
                 b.putInt(r.getOffsetPointer());
             }
             raf.write(b.array());
-           // byte[] freeBytes = new byte[RECORD_SIZE];
-          //  for (int j=0; j<RECORD_SIZE; j++)freeBytes[j]=(byte)0xFF;
-          //  for(int i=page.size(); i < BLOCKING_FACTOR; i++){
-          //      raf.write(freeBytes);
-          //  }
-        }catch (IOException e) {
+            raf.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public ArrayList<Record> getPage(int pageNo){
-        File f = new File(dataFileName);
+    public ArrayList<Record> getPage(int pageNo) {
+        File f = new File(dataFile);
         ArrayList<Record> page = new ArrayList<>();
         try {
             f.createNewFile();
             RandomAccessFile raf = new RandomAccessFile(f, "rw");
-            raf.seek((pageNo-1) * BLOCKING_FACTOR * RECORD_SIZE);
+            raf.seek((pageNo - 1) * BLOCKING_FACTOR * RECORD_SIZE);
             byte[] bytes = new byte[BLOCKING_FACTOR * RECORD_SIZE];
             int bytesRead = raf.read(bytes);
-            for(int i=0; i<bytesRead; i+=RECORD_SIZE){
-                int key = (bytes[i] << 24) | ((bytes[i+1]&0xFF) << 16) | ((bytes[i+2]&0xFF) << 8) | (bytes[i+3] & 0xFF);
-                if(key==-1 || key==0)continue;
+            for (int i = 0; i < bytesRead; i += RECORD_SIZE) {
+                int key = (bytes[i] << 24) | ((bytes[i + 1] & 0xFF) << 16) | ((bytes[i + 2] & 0xFF) << 8) | (bytes[i + 3] & 0xFF);
+                if (key == 0) continue;
                 StringBuilder sb = new StringBuilder("");
-                for(int j=0; j<60; j++){
-                    if(bytes[i+4+j] == 0)continue;
-                    sb.append((char)bytes[i+4+j]);
+                for (int j = 0; j < 60; j++) {
+                    if (bytes[i + 4 + j] == 0) continue;
+                    sb.append((char) bytes[i + 4 + j]);
                 }
                 boolean isDeleted = bytes[64] == 1;
-                int offsetPointer = (bytes[i+65] << 24) | ((bytes[i+66]&0xFF) << 16) | ((bytes[i+67]&0xFF) << 8) | (bytes[i+68] & 0xFF);
+                int offsetPointer = (bytes[i + 65] << 24) | ((bytes[i + 66] & 0xFF) << 16) | ((bytes[i + 67] & 0xFF) << 8) | (bytes[i + 68] & 0xFF);
                 page.add(new Record(key, sb.toString().toCharArray(), isDeleted, offsetPointer));
             }
-        }catch (IOException e) {
+            raf.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return page;
     }
 
-    private int indexLookup(int key){
+    private int indexLookup(int key) {
         int lastKey = -1;
-        for (IndexEntry i : index){
-           // if(key<=index.get(0).getKey())return 1;
-            if(lastKey == -1){
+        for (IndexEntry i : index) {
+            if (lastKey == -1) {
                 lastKey = i.getKey();
                 continue;
             }
-            if(i.getKey() > key && lastKey <= key) return i.getPageNo()-1;
+            if (i.getKey() > key && lastKey <= key) return i.getPageNo() - 1;
             lastKey = i.getKey();
         }
         return index.size();
--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void parse(String s, ISAM isam){
+    private static void parse(String s, ISAM isam){
+        if (s.equals("")){
+            return;
+        }
         switch (s.charAt(0)){
             case 'i':
-                isam.insert(new Record(s.substring(2).toCharArray()));
+                String[] params = s.split(" ");
+                if(params.length == 2)
+                    isam.insert(new Record(params[1].toCharArray()));
+                else System.err.println("Wrong \'i\' command format");
                 break;
-            case 'r':
-                //remove()
+            case 'd':
+                //delete()
                 break;
             case 'u':
                 //update()
+                break;
+            case 'r':
+                isam.reorganize();
                 break;
             case 'p':
                 printDataFile(isam);
@@ -28,20 +37,32 @@ public class Main {
 
     public static void printDataFile(ISAM isam){
         ArrayList<Record> page;
-        System.out.println("============ PRIMARY AREA ============");
+        System.out.println("============ PRIMARY AREA =============");
         for(int i=1; i<=isam.getPagesNumber(); i++){
             page = isam.getPage(i);
             if(page.size() == 0)break;
-            if(i> isam.getIndex().size())System.out.println("============ OVERFLOW AREA ============");
+            if(i> isam.getIndex().size())System.out.println("\n============ OVERFLOW AREA ============");
             System.out.println("Page "+i+ ": ");
             for(Record r: page){
-                System.out.print("Key: "+r.getKey()+" Data: "+ Arrays.toString((r.getCharset())));
-                System.out.println(" Offset Pointer: " +r.getOffsetPointer() + " IsDeleted: "+r.isDeleted());
+                System.out.print("Key: "+r.getKey()+" | Data: "+ Arrays.toString((r.getCharset())));
+                System.out.println(" | Offset Pointer: " +r.getOffsetPointer() + " | IsDeleted: "+r.isDeleted());
             }
+        }
+        System.out.println("\n================ INDEX ================");
+        for(IndexEntry e: isam.getIndex()){
+            System.out.println("Key: "+e.getKey()+" | Page Number: "+ e.getPageNo());
         }
     }
 
     public static void handleMenu(ISAM isam){
+        System.out.println("List of available commands:");
+        System.out.println("\'i\' followed by record content (string) -> Insert record containing specified value");
+        System.out.println("\'d\' followed by key (integer) -> Delete record with specified key");
+        System.out.println("\'p\' -> Print content of primary area, overflow area and index");
+        System.out.println("\'r\' -> Force reorganization");
+        System.out.println("\'u\' followed by key (integer) followed by record content (string)" +
+                " -> Update record pointed by key with specified content");
+        System.out.println("\'q\' -> Save changes to disk and quit program");
         Scanner in = new Scanner(System.in);
         String line;
         do {
@@ -49,12 +70,12 @@ public class Main {
             parse(line, isam);
         }
         while(!line.equals("q"));
+        isam.saveIndex();
     }
 
     public static void main(String[] args) {
         ISAM isam = new ISAM();
         handleMenu(isam);
-        //ArrayList<Record> page = b.getPage(0);
     }
 
 
